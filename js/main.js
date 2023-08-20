3dd530eda592aef6f646d3ce2339a75c5c8ea62e
Haris <haris.usman@hotmail.com>
2023-08-20 20:44:58 +0200

    Changed paths for gsap as well

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "/node_modules/three/build/three.module.js";
 import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls";
-import gsap from "gsap";
+import { gsap } from "/node_modules/gsap";
+
 // import {
 //   CSS3DRenderer,
 //   CSS3DObject,
